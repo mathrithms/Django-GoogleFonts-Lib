2bb887e58bf3e55e48483d5aab7ed9501a241d72
ksonawat007 <ssks81285@gmail.com>
2021-03-07 01:09:54 +0530

    Added subsets and files as ManyToManyField

--- fonts/databasedesign/admin.py
+++ fonts/databasedesign/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import font
 # Register your models here.
-admin.site.register(font)
+@admin.register(font)
+
+class fontName(admin.ModelAdmin):
+	list_display = ["category", "kind", "family", "version", "lastModified"]
--- fonts/databasedesign/models.py
+++ fonts/databasedesign/models.py
@@ -7,22 +7,31 @@ import requests
 class font(models.Model):
 	category = models.CharField(max_length = 20, default = "sans-serif")
 	kind = models.CharField(max_length = 20, default = 'webfonts#webfont')
-	family	= models.CharField("family", max_length = 25, default = "Arial")
-	variants = models.CharField(max_length = 25, null = True)
+	family	= models.CharField("family", max_length = 500, default = "Arial")
 	version = models.CharField(max_length = 20, default = "v12")
-	lastModified = models.DateField()
-	files = models.CharField(max_length = 300, null = True)
-	#subsets = models.CharField(max_length = 25, null = True)
+	lastModified = models.DateField(default = datetime.date(2020,1,1))
+	files = models.ManyToManyField("variants")
+	subsets = models.ManyToManyField("subsets")
 
 
-def json_response(API_KEY):
-	response = requests.get("https://www.googleapis.com/webfonts/v1/webfonts?key=" + API_KEY).text
-	py_response = json.loads(response)
-	for i in py_response['items'][:2]:
-		for variant in i['variants']:
-			data =  font.objects.create(category = i['category'], family = i['family'], files = i['files'][variant],
-										version = i['version'], kind = i['kind'], variants = variant,
-										lastModified = i['lastModified']
-										)
-			data.save()
+
+class variants(models.Model):
+		files = models.URLField(max_length = 300, null = True)
+		def __str__(self):
+			return self.files
+
+
+class subsets(models.Model):
+	subsets = models.CharField(max_length = 25, blank = True)
+	def __str__(self):
+		return self.subsets
+# def json_response(API_KEY):
+# 	response = requests.get("https://www.googleapis.com/webfonts/v1/webfonts?key=" + API_KEY).text
+# 	py_response = json.loads(response)
+# 	for i in py_response['items'][:2]:
+# 		for variant in i['variants']:
+# 			data =  font.objects.create(category = i['category'], family = i['family'], files = i['files'][variant],
+# 										version = i['version'], kind = i['kind'], lastModified = i['lastModified']
+# 										)
+# 			data.save()
 
